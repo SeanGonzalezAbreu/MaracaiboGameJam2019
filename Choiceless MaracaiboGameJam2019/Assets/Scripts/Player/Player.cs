955bfcfed05c62f7043ad420d8178737f7c5625b
Sean Gonzalez <seangonzalez55@gmail.com>
2019-01-25 23:58:41 -0400

    Setting Up the Repo!
    Basic Folder Structure

---
+++ Choiceless MaracaiboGameJam2019/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Choiceless.Scripts.Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Player : MonoBehaviour
+    {
+        #region Variables
+        [Range(1, 100)]
+        [SerializeField]
+        private float movementSpeed;
+
+        #endregion
+
+        #region Unity Callbacks
+
+        private void Start()
+        {
+
+        }
+
+        private void Update()
+        {
+
+        }
+
+        #endregion
+
+        #region Class Methods
+
+        #endregion
+    }
+}
+//By Sean González
